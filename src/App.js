9579091f47c36dfd7e9bb99f8c3035b892be1e01
jk1dd <portajonk@gmail.com>
2017-07-21 17:13:12 -0600

    filters by name only

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class OrgRow extends React.Component {
+class OrgRow extends Component {
   render () {
     return (
       <tr>
@@ -14,12 +14,20 @@ class OrgRow extends React.Component {
   }
 }
 
-class OrgTable extends React.Component {
+class OrgTable extends Component {
   render () {
     const rows = [];
-    this.props.orgs.forEach(function(org) {
+
+    this.props.orgs.forEach((org) => {
+      if (org.name.indexOf(this.props.filterText) === -1) {
+        return;
+      }
       rows.push(<OrgRow org={org} key={org.name}/> );
     });
+    // this.props.orgs.forEach(function(org) {
+    //   rows.push(<OrgRow org={org} key={org.name}/> );
+    // });
+
     return(
       <table>
         <thead>
@@ -37,17 +45,24 @@ class OrgTable extends React.Component {
   }
 }
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this);
+  }
+  handleFilterTextInputChange(e) {
+    this.props.onFilterTextInput(e.target.value);
+  }
   render () {
     return (
       <form>
-        <input type="text" placeholder="Something Cool"/>
+      <input type="text" placeholder="Search..." value={this.props.filterText} onChange={this.handleFilterTextInputChange}/>
       </form>
     );
   }
 }
 
-class TableInfoText extends React.Component {
+class TableInfoText extends Component {
   render () {
     return (
       <h2>Look at all these organizations!</h2>
@@ -55,13 +70,31 @@ class TableInfoText extends React.Component {
   }
 }
 
-class WholeApp extends React.Component {
+class WholeApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: ''
+    };
+    this.handleFilterTextInput = this.handleFilterTextInput.bind(this);
+  }
+
+  handleFilterTextInput(filterText) {
+    this.setState({
+      filterText: filterText
+    });
+  }
   render () {
     return (
       <div>
         <TableInfoText />
-        <SearchBar />
-        <OrgTable orgs={this.props.orgs} />
+        <SearchBar
+          filterText={this.state.filterText}
+          onFilterTextInput={this.handleFilterTextInput}
+        />
+        <OrgTable
+          orgs={this.props.orgs}
+          filterText={this.state.filterText}/>
       </div>
     );
   }
