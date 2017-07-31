7fa4f7c135466b2e82a9a4d32f5387164f0dd9b3
jk1dd <portajonk@gmail.com>
2017-07-31 11:16:43 -0600

    map renders, no information, missing key

--- src/map/map.jsx
+++ src/map/map.jsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 
-const Map = () => (
-  <div className='map-things'>
-    <div className='tab-header' id='map-block'>
-      <h1>Mapped</h1>
-      <p>Look at organizations on the map!</p>
-    </div>
-    <div className='off-the-wall'>
+// const Map = () => (
+//   <div className='map-things'>
+//     <div className='tab-header' id='map-block'>
+//       <h1>Mapped</h1>
+//       <p>Look at organizations on the map!</p>
+//     </div>
+//     <div className='off-the-wall'>
+//
+//     </div>
+//   </div>
+// )
 
-    </div>
-  </div>
-)
+class Map extends Component {
 
-export default Map;
+  constructor(){
+    super()
+    this.state = {
+      map: null
+    }
+  }
+
+  mapMoved () {
+    console.log('Map moved')
+  }
+
+  mapLoaded(map) {
+    // console.log('mapLoaded:' + JSON.stringify(map.getCenter()))
+    if (this.state != null)
+    return this.setState({
+      map: map
+    })
+  }
+  render () {
+    const markers = this.props.markers || []
+
+    return (
+      <GoogleMap
+        ref={this.mapLoaded.bind(this)}
+        onDragEnd={this.mapMoved.bind(this)}
+        defaultZoom={this.props.zoom}
+        defaultCenter={this.props.center} >
+        {markers.map((marker, index) => (
+          <Marker {...marker} />
+        )
+      )}
+      </GoogleMap>
+    )
+  }
+}
+
+export default withGoogleMap(Map);
