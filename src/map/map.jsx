dcfcf21f784c38ba30754de78704c41474bc0e1d
jk1dd <portajonk@gmail.com>
2017-07-31 16:56:48 -0600

    remove link reference in map.js

--- src/map/map.jsx
+++ src/map/map.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps'
 import axios from 'axios'
