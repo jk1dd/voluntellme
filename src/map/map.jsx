a5f9a7f879f1c9d5f92cc8c8910048cf01b3bf54
jk1dd <portajonk@gmail.com>
2017-07-31 15:56:06 -0600

    add info windows with org name, no key

--- src/map/map.jsx
+++ src/map/map.jsx
@@ -3,18 +3,6 @@ import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps'
 import axios from 'axios'
 const google = window.google;
 
-// const Map = () => (
-//   <div className='map-things'>
-//     <div className='tab-header' id='map-block'>
-//       <h1>Mapped</h1>
-//       <p>Look at organizations on the map!</p>
-//     </div>
-//     <div className='off-the-wall'>
-//
-//     </div>
-//   </div>
-// )
-
 class Map extends Component {
 
   constructor(){
@@ -25,33 +13,42 @@ class Map extends Component {
     }
   }
 
-  // componentDidMount() {
-  //   axios.get('http://localhost:3000/api/v1/organizations').then((result) => {
-  //     this.setState({ markers: result.data.map(org => {
-  //       return {lat: org.loc.replace(/[()]/g, '').split(',')[0], lng: org.loc.replace(/[()]/g, '').split(',')[1]}
-  //       })
-  //     });
-  //   })
-  // }
-
   componentDidMount() {
     axios.get('http://localhost:3000/api/v1/organizations').then((result) => {
       this.setState({ organizations: result.data });
     });
   }
 
-  // mapMoved () {
-  //   console.log('Map moved')
-  // }
-  //
-  // mapLoaded(map) {
-  //   // console.log('mapLoaded:' + JSON.stringify(map.getCenter()))
-  //   // debugger
-  //   if (this.state != null)
-  //   return this.setState({
-  //     map: map
-  //   })
-  // }
+  handleMarkerClick = this.handleMarkerClick.bind(this)
+  handleMarkerClose = this.handleMarkerClose.bind(this)
+
+  handleMarkerClick(targetOrg) {
+    this.setState({
+      organizations: this.state.organizations.map(org => {
+        if (org === targetOrg) {
+          return {
+            ...org,
+            showInfo: true,
+          }
+        }
+        return org
+      })
+    })
+  }
+
+  handleMarkerClose(targetOrg) {
+    this.setState({
+      organizations: this.state.organizations.map(org => {
+        if (org === targetOrg) {
+          return {
+            ...org,
+            showInfo: false,
+          }
+        }
+        return org
+      })
+    })
+  }
 
   render () {
 
@@ -62,9 +59,20 @@ class Map extends Component {
         {this.state.organizations.map(org => (
           <Marker
             position={new google.maps.LatLng(org.loc.replace(/[()]/g, '').split(',')[0], org.loc.replace(/[()]/g, '').split(',')[1].substring(1))}
-            key={org.id} />
-        )
-      )}
+            key={org.id}
+            showInfo={false}
+            onClick={() => this.handleMarkerClick(org)}
+             >
+
+             {org.showInfo && (
+              <InfoWindow onCloseClick={() => this.handleMarkerClose(org)}>
+                <div>{org.name}</div>
+              </InfoWindow>
+            )}
+
+          </Marker>
+          )
+        )}
       </GoogleMap>
     )
   }
