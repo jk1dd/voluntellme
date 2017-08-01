c817ab950de2b0275f5c49f9f87f3f07257357ef
jk1dd <portajonk@gmail.com>
2017-07-31 21:48:30 -0600

    begin styling for info page

--- src/info/info.jsx
+++ src/info/info.jsx
@@ -9,7 +9,7 @@ const Info = () => (
     <div className='off-the-wall'>
       <div className='who-info'>
         <h2>Who volunteers?</h2>
-        <p>People all over the country volunteer. You should too.</p>
+        <p>People all over the country volunteer. You should too. It doesn't matter what age or stage of life you are in - there is always something to help with!</p>
       </div>
       <div className='what-info'>
         <h2>What sorts of things can you do?</h2>
@@ -18,19 +18,21 @@ const Info = () => (
           <li>Homeless</li>
           <li>Elderly</li>
           <li>Social Issues</li>
+          <li>Education</li>
+          <li>Health</li>
         </ul>
       </div>
       <div className='why-info'>
         <h2>What are some reasons to volunteer?</h2>
-        <p>It is good for everyone.</p>
+        <p>It is good for everyone! It can make you happier and heathier, more connected to others and to your community, and bring you enjoyment. At the same time, it makes your community and country a better place to live in, and those who volunteer are more personally invested in the community and its success.</p>
       </div>
       <div className='when-info'>
         <h2>When should you do it?</h2>
-        <p>Anytime is a good time.</p>
+        <p>Anytime is a good time! There are so many different types of activities and ways to get involved, that there is always a way to fit in volunteering in your life. The options range from months-long (or even years-long!) commitments, all the way down to just a few hours on a Saturday afternoon.</p>
       </div>
       <div className='how-info'>
         <h2>Want more info?</h2>
-        <a href="https://www.nationalservice.gov/resources/recruitment/volunteering-america-resources">Lots of resources here</a>
+        <a href="https://www.nationalservice.gov/resources/recruitment/volunteering-america-resources">Check out some of the resources and information listed at this site!</a>
       </div>
     </div>
   </div>
